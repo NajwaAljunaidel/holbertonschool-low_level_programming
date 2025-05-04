d98342079a7b97beef9fb690ff5f812a9e0cd92a
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-05-04 14:58:46 +0000

    f

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(void){
+/**
+ *
+ * main print a texttt
+ *
+ *
+ *
+ *
+ * / 
+int main(void)
+{
 
         printf("with proper grammar, but the outcome is a piece of art,/n"); 
         return (0); 

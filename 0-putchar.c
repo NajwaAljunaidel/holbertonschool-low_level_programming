ed297d28f1b10fd3e8a6f65af92062cb94984768
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-05-04 16:48:16 +0000

    f

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  // تضمين مكتبة الإدخال والإخراج القياسية
+
+/**
+ * main - نقطة الدخول للبرنامج
+ * 
+ * Return: دائمًا 0 (نجاح)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return 0;
+}

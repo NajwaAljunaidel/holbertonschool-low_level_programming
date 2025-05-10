125e68267330c7dc4d312de26977fc07ce0e6402
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-05-10 20:04:34 +0000

    FF

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,17 +1,13 @@
 #include "main.h"
-/** 
- * Write a function that multiplies two integers.
- */ goood jub
 
-int mul(int a, int b)	
-}
-
-
-       return (a*b);
-
-
-
-
-
-
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
 {
+	return (a * b);
+}

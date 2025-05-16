06086b7cb0aebf3ec3ca026eadd33f9db9cd46a2
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-05-16 23:19:25 +0000

    programming c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+
+
+char *_memset(char *s, char b, unsigned int n); 
+
+{ 
+
+	unsigned int itre;
+
+	for(itre =0; n>0; n--)
+	{
+	
+		s[itre] = b;
+
+
+	}
+
+	return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

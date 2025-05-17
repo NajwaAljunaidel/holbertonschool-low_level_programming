e22e736c49f387eb8471877c34afb1e5a8a17a97
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-05-17 01:37:20 +0000

    c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,20 +3,22 @@
 /**
  * _memcpy - copies memory area
  * @src: memory area to copy from
- * @dest:  array that i want copy in
+ * @dest: array that i want copy in
  * @n: the length of src to be copied to dest
  * Return: pointer dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i; 
-	for (int i = 0; i < n; i++) 
-	{
+	unsigned int i;
 
- 
-		dest[i] = src[i]; 
-	
-        }
+
+	for (i = 0; i < n; i++)
+
+
+		dest[i] = dest[src];
+
+
 	return (dest);
+
 }

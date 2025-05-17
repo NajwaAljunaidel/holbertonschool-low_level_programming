bd50585b23ed67c7b11a9df121f2cb05ffe5c87a
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-05-17 02:47:46 +0000

    c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - said where this charecter in a string
+ * @c: string we search for
+ * @s: the string that we will search in
+ * Return: if the c is found -pointer to the first occurrence if c  not - null
+ *
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+
+{
+        int index;
+
+        for (index = 0; s[index] >= '\0'; index++)
+        {
+                if (s[index] == c)
+                        return (s + index);
+        }
+
+        return ('\0');
+}

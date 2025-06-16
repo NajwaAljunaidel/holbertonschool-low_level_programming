023a8ce49ab24b35432dfeaf280bfcba7c2bdd5b
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-06-16 10:34:22 +0000

    zero

---
+++ singly_linked_lists/0-malloc_checked.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+ 
+
+ size_t print_list(const list_t *h) {
+
+
+
+   size_t count = 0;
+
+   while(h != NULL) {
+
+      if(h->str == NULL) 
+      printf("[0] (nil)");
+
+      else
+      printf("[%u] %s \n" , h->len , h->str);
+
+// %u is for unsigned int: it only prints positive numbers, never negative
+
+
+      count++;
+      h = h->next;
+
+
+
+
+
+   } return count;
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+ }

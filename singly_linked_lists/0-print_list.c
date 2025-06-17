2704e52d315c587629317563cec1778a75e2cecc
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-06-17 15:34:46 +0000

    c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-i#include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
@@ -9,29 +9,24 @@ i#include <stdlib.h>
  *
  * Return: Always 0.
  */
-size_t print_list(const list_t *h) {
-
-
+size_t print_list(const list_t *h)
+{
 size_t count = 0;
 
-while(h != NULL) {
-
+while(h != NULL)
+{
 if(h->str == NULL)
 printf("[0] (nil)");
 
 else
 printf("[%u] %s \n" , h->len , h->str);
 
-// %u is for unsigned int: it only prints positive numbers, never negative
+
 
 
 count++;
 h = h->next;
 
-
-
-
-
-} return count;
-
+}
+return (count);
 }

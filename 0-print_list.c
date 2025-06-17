c1defd97e0294dff9b46bcad8ad2ce10f93cdae0
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-06-17 15:29:21 +0000

    c

--- 0-print_list.c
+++ 0-print_list.c
@@ -9,48 +9,24 @@
  *
  * Return: Always 0.
  */
-
- 
-
- size_t print_list(const list_t *h) {
+size_t print_list(const list_t *h) {
 
 
 
-   size_t count = 0;
+size_t count = 0;
 
-   while(h != NULL) {
+while(h != NULL) {
+if(h->str == NULL)
+printf("[0] (nil)");
 
-      if(h->str == NULL) 
-      printf("[0] (nil)");
-
-      else
-      printf("[%u] %s \n" , h->len , h->str);
+else
+printf("[%u] %s \n" , h->len , h->str);
 
 // %u is for unsigned int: it only prints positive numbers, never negative
 
+count++;
+h = h->next;
 
-      count++;
-      h = h->next;
-
-
-
-
-
-   } return count;
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
- }
+}
+return (count);
+}
--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+i#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
@@ -9,48 +9,29 @@
  *
  * Return: Always 0.
  */
-
- 
-
- size_t print_list(const list_t *h) {
+size_t print_list(const list_t *h) {
 
 
+size_t count = 0;
 
-   size_t count = 0;
+while(h != NULL) {
 
-   while(h != NULL) {
+if(h->str == NULL)
+printf("[0] (nil)");
 
-      if(h->str == NULL) 
-      printf("[0] (nil)");
-
-      else
-      printf("[%u] %s \n" , h->len , h->str);
+else
+printf("[%u] %s \n" , h->len , h->str);
 
 // %u is for unsigned int: it only prints positive numbers, never negative
 
 
-      count++;
-      h = h->next;
+count++;
+h = h->next;
 
 
 
 
 
-   } return count;
+} return count;
 
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
- }
+}

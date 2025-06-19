6480ca8faa3cff1b3dbbd9ee8142dba1cce49f35
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-06-19 06:53:03 +0000

    c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,30 +1,29 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - dog class
- *
- * @name: Name of the dog
- *
- * @age: Age of the dog
- *
- * @owner: Owner of the dog
- */
+* struct dog - basic dog info
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*/
+
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
 
 #endif
